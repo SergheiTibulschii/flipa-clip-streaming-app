18603472c01e616111452ea4fb3a4e5ebb7908c4
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-08-16 14:41:45 +0300

    update

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -39,7 +39,13 @@ export const Header = () => {
   };
 
   return (
-    <nav className="sticky top-0 z-10 bg-dark left-0 right-0 isolate" ref={ref}>
+    <nav
+      style={{
+        top: 'env(safe-area-inset-top, 0px)',
+      }}
+      className="sticky z-10 bg-dark left-0 right-0 isolate"
+      ref={ref}
+    >
       <Container>
         <div className="flex items-center">
           <div className="basis-1/6">
