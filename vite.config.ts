96b7064f3fb16109916ccc845a0fe10a47c2bae2
Serghei Tibulschi <serghei@thenoughtyfox.com>
2024-09-09 22:32:50 +0300

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api-qa.flipaclip.com/',
-        secure: false,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [svgr(), react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_V1,
+          secure: false,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  };
 });
